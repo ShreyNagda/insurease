da3943c727dabefbc8893340a9df2ade54f42e04
Shrey Nagda <shreynagda2714@gmail.com>
2025-06-28 15:17:10 +0530

    Added rate limiting

--- src/app/api/explain/route.ts
+++ src/app/api/explain/route.ts
@@ -1,8 +1,30 @@
 import { AiTextAnalysis } from "@/lib/AI";
 import { extractTextFromPDF } from "@/lib/pdfParser";
 import { NextRequest, NextResponse } from "next/server";
+import { RateLimiterMemory } from "rate-limiter-flexible";
+
+const rateLimiter = new RateLimiterMemory({
+  points: 1, // 1 request
+  duration: 10, // per 10 seconds
+});
 
 export async function POST(req: NextRequest) {
+  const ip = req.headers.get("x-forwarded-for") || "unknown";
+
+  try {
+    // Apply rate limit
+    await rateLimiter.consume(ip);
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      {
+        error:
+          "Too many requests. Please wait 10 seconds before uploading again.",
+      },
+      { status: 429 }
+    );
+  }
+
   const formData = await req.formData();
   const file = formData.get("file") as File;
 
